e9b8827b17b5c04a0e771b5686ede0a7f301ebc8
Ilya Kolbin <iskolbin@gmail.com>
2015-07-28 22:44:06 +0300

    -s flag

--- unpack_littera.py
+++ unpack_littera.py
@@ -20,13 +20,24 @@ def process_littera( dom ): return {
     'kerningscount': dom.find('kernings') != None and xint( dom.find('kernings').attrib['count'] ),
 }
 
-def default_get_path( littera, char, charinfo ):
+def default_get_path_codes( littera, char, charinfo ):
     return '%s_%d.png' % (littera['info']['face'], charinfo['id'])
+
+special_symbols = {
+    '.': 'dot',
+    ',': 'comma',
+    '\\': 'backslash',
+    '/': 'slash',
+    '&': 'ampersand',
+}
+
+def default_get_path_symbols( littera, char, charinfo ):
+    return '%s_%s.png' % (littera['info']['face'], special_symbols.get( char, char ))
 
 def get_max_char_height( littera ):
     return max(char['height']+char['yoffset'] for char in littera['chars'].values())
 
-def unpack_littera_page( littera, page, get_path=default_get_path ):
+def unpack_littera_page( littera, page, get_path=default_get_path_codes ):
     im = img.open( littera['pages'][page]['file'] )
     h = get_max_char_height( littera )
     for k,v in dict((k,v) for k, v in littera['chars'].items() if v['page'] == page and v['width'] > 0 and v['height'] > 0).items():
@@ -36,16 +47,18 @@ def unpack_littera_page( littera, page, get_path=default_get_path ):
         ires.save( get_path(littera,k,v))
     
 
-def unpack_littera_font( xmlfont ):
+def unpack_littera_font( xmlfont, symbolic ):
     littera = process_littera( et.parse( xmlfont ))
     print( get_max_char_height( littera ))
-    unpack_littera_page( littera, 0 )
+    unpack_littera_page( littera, 0, symbolic and default_get_path_symbols or default_get_path_codes )
 
 if __name__=='__main__':
     if len( sys.argv ) <= 1:
-        print('Usage: ./unpack_littera.py <font-file>.fnt')
+        print('Usage: ./unpack_littera.py <font-file>.fnt [-s]')
         print('Unpack littera-created bitmap fonts into separate images')
         print('See http://kvazars.com/littera/')
         print('Save font in XML(.fnt) format')
+        print('Output file format is <font-face>_<char-code>.png')
+        print('  if -s flag is used then <font-face>_<char>.png')
     else:
-        unpack_littera_font( sys.argv[1] )
+        unpack_littera_font( sys.argv[1], '-s' in sys.argv )
