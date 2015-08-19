9af71eb3e220a08890289e3bcf14cffccf83d75f
Ilya Kolbin <iskolbin@gmail.com>
2015-08-19 21:54:14 +0300

    diviable by 2 support

--- unpack_littera.py
+++ unpack_littera.py
@@ -37,28 +37,34 @@ def default_get_path_symbols( littera, char, charinfo ):
 def get_max_char_height( littera ):
     return max(char['height']+char['yoffset'] for char in littera['chars'].values())
 
-def unpack_littera_page( littera, page, get_path=default_get_path_codes ):
+def unpack_littera_page( littera, page, get_path=default_get_path_codes, common_divisor=(1,1) ):
     im = img.open( littera['pages'][page]['file'] )
     h = get_max_char_height( littera )
+    while h % common_divisor[1] != 0: h = h + 1
     for k,v in dict((k,v) for k, v in littera['chars'].items() if v['page'] == page and v['width'] > 0 and v['height'] > 0).items():
         icrop = im.crop((v['x'],v['y'],v['x']+v['width'],v['y']+v['height']))
-        ires = img.new( 'RGBA', (v['width'],h))
+        w = v['width']
+        while w % common_divisor[0] != 0: w = w + 1
+        ires = img.new( 'RGBA', (w,h))
         ires.paste( icrop, (0,v['yoffset']))
         ires.save( get_path(littera,k,v))
-    
 
-def unpack_littera_font( xmlfont, symbolic ):
+def unpack_littera_font( xmlfont, symbolic, common_divisor ):
     littera = process_littera( et.parse( xmlfont ))
     print( get_max_char_height( littera ))
-    unpack_littera_page( littera, 0, symbolic and default_get_path_symbols or default_get_path_codes )
+    unpack_littera_page( littera, 0, symbolic and default_get_path_symbols or default_get_path_codes, common_divisor )
 
 if __name__=='__main__':
     if len( sys.argv ) <= 1:
-        print('Usage: ./unpack_littera.py <font-file>.fnt [-s]')
+        print('Usage: ./unpack_littera.py <font-file>.fnt [-s] [-w2] [-h2]')
         print('Unpack littera-created bitmap fonts into separate images')
         print('See http://kvazars.com/littera/')
         print('Save font in XML(.fnt) format')
         print('Output file format is <font-face>_<char-code>.png')
-        print('  if -s flag is used then <font-face>_<char>.png')
+        print('Flags:')
+        print('-s   output file format is <font-face>_<char>.png')
+        print('-w2  make width dividable by 2' )
+        print('-h2  make height dividable by 2' )
+        print('-2   make width and height dividable by 2' )
     else:
-        unpack_littera_font( sys.argv[1], '-s' in sys.argv )
+        unpack_littera_font( sys.argv[1], '-s' in sys.argv, (('-w2' in sys.argv or '-2' in sys.argv) and 2 or 1, ('-h2' in sys.argv or '-2' in sys.argv) and 2 or 1) )
